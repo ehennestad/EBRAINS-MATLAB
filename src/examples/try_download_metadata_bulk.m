74b39a0cb850b0ae59512a2067d5fb9f2e8afd76
ehennestad <ehennestad@gmail.com>
2025-04-07 21:28:41 +0200

    Update try_download_metadata_bulk.m

--- src/examples/try_download_metadata_bulk.m
+++ src/examples/try_download_metadata_bulk.m
@@ -1,3 +1,7 @@
+kg2OmIdentifierMap = ebrains.kg.kg2openminds.internal.getIdentifierMapping();
+controlledKgIds = kg2OmIdentifierMap.keys();
+
+
 tic
 dsvIdentifier = '8f36d73a-befc-4aa5-b501-fc546e723f30';
 
@@ -16,6 +20,7 @@ jsonLdStr = openminds.internal.serializer.struct2jsonld(omNode);
 
 pattern = '(?<="@id":\s*")[^"]+(?=")';
 IRIs = regexp(jsonLdStr, pattern, "match");
+IRIs = setdiff(IRIs, controlledKgIds);
 
 kgNodes = ebrains.kg.downloadInstancesBulk(IRIs);
 disp(numel(kgNodes))
@@ -27,12 +32,17 @@ newIRIs = regexp(jsonLdStr, pattern, "match");
 newIRIs = unique(newIRIs);
 
 missingIRIs = setdiff(newIRIs, IRIs);
+missingIRIs = setdiff(missingIRIs, controlledKgIds);
+
 kgNodes = ebrains.kg.downloadInstancesBulk(missingIRIs);
 kgNodes(cellfun('isempty', kgNodes))=[];
 missingNodes = ebrains.kg.kg2openminds.internal.convertKgNode(kgNodes);
 disp(numel(missingNodes))
 omNodes = [{omNode}, omNodes, missingNodes];
-             
+
+% replace all controlled instance uuids
+omNodes = ebrains.kg.kg2openminds.internal.replaceControlledInstanceIds(omNodes, kg2OmIdentifierMap);
+
 jsonInstance = openminds.internal.serializer.struct2jsonld(omNodes);
 openminds.internal.utility.filewrite('test_kg_download.jsonld', jsonInstance)
 toc
