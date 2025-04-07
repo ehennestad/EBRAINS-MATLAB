bf5d05bc4deb3f1ed5315ba851c10d53559729db
ehennestad <ehennestad@gmail.com>
2025-04-07 20:39:11 +0200

    Update convertKgNode.m
    Update to resolve recursively (for embeded nodes)

--- src/+ebrains/+kg/+kg2openminds/+internal/convertKgNode.m
+++ src/+ebrains/+kg/+kg2openminds/+internal/convertKgNode.m
@@ -2,8 +2,12 @@ function omNode = convertKgNode(kgNode)
 
     if numel(kgNode) > 1
         omNode = cell(1, numel(kgNode));
+        if ~iscell(kgNode); kgNode = num2cell(kgNode); end
         for i = 1:numel(kgNode)
             omNode{i} = ebrains.kg.kg2openminds.internal.convertKgNode(kgNode{i});
+        end
+        try
+            omNode = [omNode{:}];
         end
         return
     end
@@ -13,8 +17,20 @@ function omNode = convertKgNode(kgNode)
     omNode = ebrains.kg.kg2openminds.internal.filterData(kgNode);
     omNode = ebrains.kg.kg2openminds.internal.removeContextPrefix(omNode);
     
+    propertyNames = fieldnames(omNode);
+    for i = 1:numel(propertyNames)
+        thisPropertyName = propertyNames{i};
+        % Recursively process embedded nodes
+        if isstruct(omNode.(thisPropertyName)) && ~isfield(omNode.(thisPropertyName), 'x_id')
+            thisPropertyValue = omNode.(thisPropertyName);
+            omNode.(thisPropertyName) = ebrains.kg.kg2openminds.internal.convertKgNode(thisPropertyValue);
+        end
+    end
+
     assert(iscell(type) && numel(type)==1, ...
         "Expected @type to be a cell with one element. If you see this error, please report.")
     omNode.at_type = type{1};
-    omNode.at_id = identifier;
+    if ~isempty(identifier)
+        omNode.at_id = identifier;
+    end
 end
