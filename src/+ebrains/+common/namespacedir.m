73cebdfd0c4bd644fc12f71fcbf7151550fe908c
ehennestad <ehennestad@gmail.com>
2025-04-08 09:22:49 +0200

    Create namespacedir.m

---
+++ src/+ebrains/+common/namespacedir.m
@@ -0,0 +1,21 @@
+function pathName = namespacedir(namespaceName)
+% namespacedir - Retrieves the directory path name of the given namespace
+%
+% Syntax:
+%   folderPathName = ebrains.common.namespacedir(namespace) Returns the 
+%   directory path name (location) of the given namespace.
+%
+% Output Arguments:
+%   folderPathName - A string containing the full path to the namespace folder.
+
+    arguments
+        namespaceName (1,1) string
+    end
+    
+    namespaceParts = strsplit(namespaceName, '.');
+    namespaceParts = "+" + namespaceParts;
+    namespaceRelativePath = fullfile(namespaceParts{:});
+
+    info = what(namespaceRelativePath);
+    pathName = info.path;
+end
