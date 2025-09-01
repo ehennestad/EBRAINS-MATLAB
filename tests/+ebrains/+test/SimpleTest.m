bda1e6a65f713eab441b8072b9180d24f35167d9
ehennestad <ehennestad@gmail.com>
2025-09-01 18:16:44 +0200

    Add test for constants

--- tests/+ebrains/+test/SimpleTest.m
+++
@@ -1,9 +0,0 @@
-classdef SimpleTest < matlab.unittest.TestCase
-    methods(Test)
-        function testAddition(testCase)
-            actual = 2 + 2;
-            expected = 4;
-            testCase.verifyEqual(actual, expected);
-        end
-    end
-end
