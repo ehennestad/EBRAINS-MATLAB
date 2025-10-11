c1969850f6695f61eadb254100c96598ceec285e
ehennestad <ehennestad@gmail.com>
2025-10-11 09:35:21 +0200

    Enhance ensureExpandedTypeName with openMINDS enum support
    Updated ensureExpandedTypeName to use the openMINDS type enum for expanding compact type names when the openMINDS_MATLAB toolbox is available. Added input validation, improved documentation, and error handling for cases where the toolbox is not installed.

--- src/+ebrains/+kg/+api/+internal/ensureExpandedTypeName.m
+++ src/+ebrains/+kg/+api/+internal/ensureExpandedTypeName.m
@@ -1,5 +1,31 @@
 function type = ensureExpandedTypeName(type)
-    if ~startsWith(type, 'https://openminds.ebrains.eu/core/')
-        type = "https://openminds.ebrains.eu/core/" + type;
+% ensureExpandedTypeName - Expands the type name to its full IRI
+%
+% Syntax:
+%   type = ebrains.kg.api.internal.ensureExpandedTypeName(type) expands the 
+%   type name to the full IRI if it is not already in the correct format.
+%
+% Input Arguments:
+%   type - A string representing the type name to be expanded.
+%
+% Output Arguments:
+%   type - The expanded type name in full URI format, if applicable.
+%
+% Note: If the openMINDS_MATLAB toolbox is installed, this function will use
+% the openMINDS type enum to automatically expand a compact type name.
+
+    arguments
+        type (1,1) string
+    end
+
+    if ~startsWith(type, 'https://openminds.ebrains.eu/') % Todo: get base IRI from constant
+        if exist('openminds.enum.Types', 'class') == 8
+            typeEnum = openminds.enum.Types(type);
+            type = typeEnum.TypeURI;
+        else
+            error(...
+                ['Please provide the type as a an expanded IRI using the ', ...
+                'the openMINDS type namespace IRI as a prefix.'])
+        end
     end
 end
