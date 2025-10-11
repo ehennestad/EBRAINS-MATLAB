acddd63da3c9d656bedde79b721fd5556c2658f5
ehennestad <ehennestad@gmail.com>
2025-10-11 21:06:43 +0200

    Fix indentation in TestConstants test methods
    Corrected the indentation of test methods in TestConstants.

--- tests/+ebrains/+test/TestConstants.m
+++ tests/+ebrains/+test/TestConstants.m
@@ -1,18 +1,18 @@
 classdef TestConstants < matlab.unittest.TestCase
     methods(Test)
-            function testCollabBaseUrl(testCase)
-                url = ebrains.common.constant.CollabBaseUrl();
-                testCase.verifyEqual(url, "https://wiki.ebrains.eu");
-            end
+        function testCollabBaseUrl(testCase)
+            url = ebrains.common.constant.CollabBaseUrl();
+            testCase.verifyEqual(url, "https://wiki.ebrains.eu");
+        end
 
-            function testDataProxyApiBaseUrl(testCase)
-                url = ebrains.common.constant.DataProxyApiBaseUrl();
-                testCase.verifyEqual(url, "https://data-proxy.ebrains.eu/api/v1/");
-            end
+        function testDataProxyApiBaseUrl(testCase)
+            url = ebrains.common.constant.DataProxyApiBaseUrl();
+            testCase.verifyEqual(url, "https://data-proxy.ebrains.eu/api/v1/");
+        end
 
-            function testKGCoreApiBaseURL(testCase)
-                url = ebrains.common.constant.KGCoreApiBaseURL();
-                testCase.verifyEqual(url, "https://core.kg.ebrains.eu/v3");
-            end
+        function testKGCoreApiBaseURL(testCase)
+            url = ebrains.common.constant.KGCoreApiBaseURL();
+            testCase.verifyEqual(url, "https://core.kg.ebrains.eu/v3");
+        end
     end
 end
