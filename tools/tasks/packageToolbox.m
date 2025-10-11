839f93dfe94305e18654540fec2539eedbf89d74
ehennestad <ehennestad@gmail.com>
2025-10-11 11:03:14 +0200

    Add varargin support to packageToolbox function
    The packageToolbox function now accepts additional optional arguments via varargin, allowing for greater flexibility when calling the function. Also updated the SourceFolderName parameter to 'src/ebrains'.

--- tools/tasks/packageToolbox.m
+++ tools/tasks/packageToolbox.m
@@ -1,11 +1,15 @@
-function [newVersion, mltbxPath] = packageToolbox(releaseType, versionString)
+function [newVersion, mltbxPath] = packageToolbox(releaseType, versionString, varargin)
     arguments
         releaseType {mustBeTextScalar,mustBeMember(releaseType,["build","major","minor","patch","specific"])} = "build"
         versionString {mustBeTextScalar} = "";
     end
+    arguments (Repeating)
+        varargin
+    end
 
     projectRootDirectory = ebtools.projectdir();
     [newVersion, mltbxPath] = matbox.tasks.packageToolbox(projectRootDirectory, releaseType, versionString, ...
+        varargin{:}, ...
         "ToolboxShortName", "EBRAINS_MATLAB", ...
-        "SourceFolderName", "src")
+        "SourceFolderName", "src/ebrains");
 end
