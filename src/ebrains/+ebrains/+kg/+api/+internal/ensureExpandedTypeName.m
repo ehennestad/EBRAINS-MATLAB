b6261a6a487a0731a4c59e032c2da0d78b896bd3
ehennestad <ehennestad@gmail.com>
2025-10-13 12:23:30 +0200

    Refactor ensureExpandedTypeName to improve IRI handling
    Renamed the output variable to typeIRI for clarity and updated documentation accordingly. Improved error handling and messaging when expanding type names, and refactored logic to use a namespace constant. The function now provides clearer feedback if the type cannot be expanded or if openMINDS_MATLAB is not available.

--- src/ebrains/+ebrains/+kg/+api/+internal/ensureExpandedTypeName.m
+++ src/ebrains/+ebrains/+kg/+api/+internal/ensureExpandedTypeName.m
@@ -1,15 +1,15 @@
-function type = ensureExpandedTypeName(type)
+function typeIRI = ensureExpandedTypeName(type)
 % ensureExpandedTypeName - Expands the type name to its full IRI
 %
 % Syntax:
-%   type = ebrains.kg.api.internal.ensureExpandedTypeName(type) expands the
+%   typeIRI = ebrains.kg.api.internal.ensureExpandedTypeName(type) expands the
 %   type name to the full IRI if it is not already in the correct format.
 %
 % Input Arguments:
 %   type - A string representing the type name to be expanded.
 %
 % Output Arguments:
-%   type - The expanded type name in full URI format, if applicable.
+%   typeIRI - The expanded type name in full IRI format, if applicable.
 %
 % Note: If the openMINDS_MATLAB toolbox is installed, this function will use
 % the openMINDS type enum to automatically expand a compact type name.
@@ -18,14 +18,27 @@ function type = ensureExpandedTypeName(type)
         type (1,1) string
     end
 
-    if ~startsWith(type, 'https://openminds.ebrains.eu/') % Todo: get base IRI from constant
-        if exist('openminds.enum.Types', 'class') == 8
-            typeEnum = openminds.enum.Types(type);
-            type = typeEnum.TypeURI;
+    openMINDSNamespaceIRI = "https://openminds.ebrains.eu"; % Todo: Get from constant
+
+    if ~startsWith(type, openMINDSNamespaceIRI)
+        if exist('openminds.enum.Types', 'class') == 8 % openMINDS_MATLAB on path?
+            try
+                typeEnum = openminds.enum.Types(type);
+                typeIRI = typeEnum.TypeURI;
+            catch
+                error(...
+                    ['The type with name "%s" was not identified as a ' ...
+                    'registered openMINDS metadata type. Ensure ', ...
+                    'openMINDS_MATLAB is up to date or check that you have ', ...
+                    'spelled the type name correctly.'], type)
+            end
         else
+            exampleTypeIRI = sprintf("%s/core/Person", openMINDSNamespaceIRI);
             error(...
-                ['Please provide the type as a an expanded IRI using the ', ...
-                'the openMINDS type namespace IRI as a prefix.'])
+                ['Type name must be specified with the openMINDS type ', ...
+                 'namespace prefix, e.g "%s"'], exampleTypeIRI) %#ok<SPERR>
         end
+    else
+        typeIRI = type;
     end
 end
